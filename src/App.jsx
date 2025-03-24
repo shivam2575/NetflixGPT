24c01d4dc487b039f427ce3912ebb962a50cb475
shivam2575 <sk2575@gmail.com>
2025-03-24 18:14:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center text-3xl font-bold underline">
+        Starting with Neflix-GPT
+      </div>
+    </>
+  );
+}
+
+export default App;
